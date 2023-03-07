e1408b2e8da39e6d590fce8a03bec90a2fbfaa96
ABITO <hancom114@gmail.com>
2023-03-07 15:10:25 +0900

    corrected w465

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
     if (session) {
       return (
         <>
-          <div className="w-[465px] relative -top-[876px] flex flex-col items-end text-white">
-            <div className="relative w-[465px] left-[180px] top-[8px] text-black"></div>
+          <div className="max-w-[465px] w-full relative -top-[876px] flex flex-col items-end text-white">
+            <div className="relative max-w-[465px] w-full left-[180px] top-[8px] text-black"></div>
             <button 
                 className="relative -top-[24px] bg-[#E1C500] text-black hover:bg-transparent text-white font-semibold hover:text-white py-2 px-4 border border-[#BFC500] hover:border-[#E1C500]"
                 onClick={() => signOut()}>{session.user!.email}
@@ -17,7 +17,7 @@ export default function Header() {
     }
     return (
       <>
-        <div className="w-[465px] relative -top-[876px] flex flex-col items-end text-white">
+        <div className="max-w-[465px] w-full relative -top-[876px] flex flex-col items-end text-white">
           <div className="relative w-[120px] -left-[75px] top-[8px] text-black"></div>
           <button 
               className="relative -top-[24px] bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-white-500 hover:border-transparent"
